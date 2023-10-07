2553ce237efc234de1dd3d028f9c0242b468f5b2
michaelpginn <milespergallon271@gmail.com>
2023-10-07 17:44:04 -0600

    Initial commit

---
+++ st_data/.ipynb_checkpoints/data-checkpoint.py
@@ -0,0 +1,34 @@
+"""Defines models and functions for loading, manipulating, and writing task data"""
+from typing import Optional, List
+import re
+
+class IGTLine:
+    """A single line of IGT"""
+    def __init__(self, transcription: str, segmentation: Optional[str], glosses: Optional[str], translation: Optional[str]):
+        self.transcription = transcription
+        self.segmentation = segmentation
+        self.glosses = glosses
+        self.translation = translation
+        self.should_segment = True
+
+    def __repr__(self):
+        return f"Trnsc:\t{self.transcription}\nSegm:\t{self.segmentation}\nGloss:\t{self.glosses}\nTrnsl:\t{self.translation}\n\n"
+
+    def gloss_list(self, segmented=False) -> Optional[List[str]]:
+        """Returns the gloss line of the IGT as a list.
+        :param segmented: If True, will return each morpheme gloss as a separate item.
+        """
+        if self.glosses is None:
+            return []
+        if not segmented:
+            return self.glosses.split()
+        else:
+            return re.split("\s|-", self.glosses)
+
+    def __dict__(self):
+        d = {'transcription': self.transcription, 'translation': self.translation}
+        if self.glosses is not None:
+            d['glosses'] = self.gloss_list(segmented=self.should_segment)
+        if self.segmentation is not None:
+            d['segmentation'] = self.segmentation
+        return d
