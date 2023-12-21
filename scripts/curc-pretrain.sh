da7208f05ad758ab04e92351f34cacab515e2714
michaelpginn <milespergallon271@gmail.com>
2023-12-21 16:05:29 -0600

    Update script

--- scripts/curc-pretrain.sh
+++ scripts/curc-pretrain.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #SBATCH --nodes=1           # Number of requested nodes
-#SBATCH --gres=gpu:v100
+#SBATCH --gres=gpu
 #SBATCH --ntasks=4          # Number of requested cores
 #SBATCH --mem=32G
-#SBATCH --time=12:00:00          # Max walltime              # Specify QOS
-#SBATCH --qos=blanca-kann
+#SBATCH --time=7-00:00:00          # Max walltime              # Specify QOS
+#SBATCH --qos=blanca-curc-gpu
+#SBATCH --partition=blanca-curc-gpu
+#SBATCH --account=blanca-curc-gpu
 #SBATCH --out=train_glosslm.%j.out      # Output file name
 #SBATCH --error=train_glosslm.%j.err
 #SBATCH --mail-type=ALL
@@ -17,4 +19,4 @@ module load anaconda
 # Run Python Script
 conda activate AutoIGT
 cd "/projects/migi8081/glosslm/src"
-python3 pretrain_multilingual_model.py --mode train --model_path
+python3 pretrain_multilingual_model.py --mode train --model_path ../models/pretrained_1
