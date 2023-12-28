218d4cc3c0a1dd676bce2eb998acbc21a5f6cc3f
michaelpginn <milespergallon271@gmail.com>
2023-12-28 15:23:10 -0600

    Fix log file name

--- scripts/curc-pretrain.sh
+++ scripts/curc-pretrain.sh
@@ -7,8 +7,8 @@
 #SBATCH --qos=blanca-curc-gpu
 #SBATCH --partition=blanca-curc-gpu
 #SBATCH --account=blanca-curc-gpu
-#SBATCH --out=train_glosslm.%j.out      # Output file name
-#SBATCH --error=train_glosslm.%j.err
+#SBATCH --out=pred_glosslm.%j.out      # Output file name
+#SBATCH --error_pred_glosslm.%j.err
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=michael.ginn@colorado.edu
 
