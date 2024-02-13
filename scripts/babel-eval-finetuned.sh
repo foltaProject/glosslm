b8f956750e8aa17fbd9f1b29113691dc2beb0057
lindiatjuatja <lindiat@andrew.cmu.edu>
2024-02-13 14:55:25 -0500

    more byt5 baseline preds

--- scripts/babel-eval-finetuned.sh
+++ scripts/babel-eval-finetuned.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=pred-gloss-lm
-#SBATCH --output ./slurm-out/pred-byt5-translation-all-finetuned-%j.out
+#SBATCH --output ./slurm-out/pred-byt5-finetuned-%j.out
 #SBATCH --nodes=1
+#SBATCH --gres=gpu:A6000:1
 #SBATCH --mem=48GB
-#SBATCH --gres=gpu:6000Ada:1
-#SBATCH --time=2:00:00
+#SBATCH --time=1:00:00
 #SBATCH --mail-user=lindiat@andrew.cmu.edu
 #SBATCH --partition=general
 
@@ -14,12 +14,19 @@ conda activate text2gloss
 
 # ft_glottocode="gitx1241"
 # lang_code="git"
+# test_split="OOD"
 
-ft_glottocode="lezg1247"
-lang_code="lez"
+ft_glottocode="dido1241"
+lang_code="ddo"
+test_split="ID"
 
-exp_name="byt5-translation-all-v2"
-pretrained_model="/data/tir/projects/tir6/general/ltjuatja/glosslm/finetuned-${lang_code}-${exp_name}/"
+# ft_glottocode="uspa1245"
+# lang_code="usp"
+# test_split="ID"
+
+# exp_name="byt5-translation-all-v2"
+exp_name="byt5-baseline-unseg"
+pretrained_model="/data/tir/projects/tir6/general/ltjuatja/glosslm/${lang_code}-${exp_name}/"
 
 cd "./src"
 python3 pretrain_multilingual_model.py \
@@ -27,8 +34,9 @@ python3 pretrain_multilingual_model.py \
     --exp_name $exp_name \
     --pretrained_model ${pretrained_model} \
     --ft_glottocode ${ft_glottocode} \
-    --test_split OOD
+    --test_split ${test_split}
+
 python3 eval.py \
-    --pred ../preds/${lang_code}-${exp_name}/test_OOD-preds.csv \
+    --pred ../preds/${lang_code}-${exp_name}/test_${test_split}-preds.csv \
     --ft_glottocode ${ft_glottocode} \
-    --test_split test_OOD
+    --test_split test_${test_split}
